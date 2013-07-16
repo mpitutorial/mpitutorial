5af907473b089cdf13f2a33bbefaafbc79ec774d
Wes Kendall <wes.kendall@akimbo.io>
2013-07-15 20:50:29 -0400

    changed modes of all files and added mpi reduce code

---
+++ mpi_reduce_allreduce/stddev.c
@@ -0,0 +1,88 @@
+// Author: Wes Kendall
+// Copyright 2013 www.mpitutorial.com
+// This code is provided freely with the tutorials on mpitutorial.com. Feel
+// free to modify it for your own use. Any distribution of the code must
+// either provide a link to www.mpitutorial.com or keep this header in tact.
+//
+// Program that computes the standard deviation of an array of elements in parallel using
+// MPI_Reduce.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+#include <math.h>
+#include <assert.h>
+  
+// Creates an array of random numbers. Each number has a value from 0 - 1
+float *create_rand_nums(int num_elements) {
+  float *rand_nums = (float *)malloc(sizeof(float) * num_elements);
+  assert(rand_nums != NULL);
+  int i;
+  for (i = 0; i < num_elements; i++) {
+    rand_nums[i] = (rand() / (float)RAND_MAX);
+  }
+  return rand_nums;
+}
+
+int main(int argc, char** argv) {
+  if (argc != 2) {
+    fprintf(stderr, "Usage: avg num_elements_per_proc\n");
+    exit(1);
+  }
+
+  int num_elements_per_proc = atoi(argv[1]);
+  // Seed the random number generator to get different results each time
+  srand(time(NULL));
+
+  MPI_Init(NULL, NULL);
+
+  int world_rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+  int world_size;
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+  // Create a random array of elements on all processes.
+  srand(world_rank);
+  float *rand_nums = NULL;
+  rand_nums = create_rand_nums(num_elements_per_proc);
+
+  // Sum the numbers locally
+  float local_sum = 0;
+  int i;
+  for (i = 0; i < num_elements_per_proc; i++) {
+    local_sum += rand_nums[i];
+  }
+
+  // Reduce all of the local sums into the global sum in order to
+  // calculate the mean
+  float global_sum;
+  MPI_Allreduce(&local_sum, &global_sum, 1, MPI_FLOAT, MPI_SUM,
+                MPI_COMM_WORLD);
+  float mean = global_sum / (num_elements_per_proc * world_size);
+
+  // Compute the local sum of the squared differences from the mean
+  float local_sq_diff = 0;
+  for (i = 0; i < num_elements_per_proc; i++) {
+    local_sq_diff += (rand_nums[i] - mean) * (rand_nums[i] - mean);
+  }
+
+  // Reduce the global sum of the squared differences to the root process
+  // and print off the answer
+  float global_sq_diff;
+  MPI_Reduce(&local_sq_diff, &global_sq_diff, 1, MPI_FLOAT, MPI_SUM, 0,
+             MPI_COMM_WORLD);
+
+  // The standard deviation is the square root of the mean of the squared
+  // differences.
+  if (world_rank == 0) {
+    float stddev = sqrt(global_sq_diff /
+                        (num_elements_per_proc * world_size));
+    printf("Mean - %f, Standard deviation = %f\n", mean, stddev);
+  }
+
+  // Clean up
+  free(rand_nums);
+ 
+  MPI_Barrier(MPI_COMM_WORLD);
+  MPI_Finalize();
+}
