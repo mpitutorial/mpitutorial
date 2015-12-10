f570830a2d6f35090eb5eeecac3c520eb32f3d69
Débora Setton <setton.debora@gmail.com>
2015-12-10 18:08:03 -0200

    Change formatting of groups.c and split.c to match other files
    1) All other C files use spaces for indentation instead of tabs;
    2) All other files use <mpi.h> instead of "mpi.h" (which makes sense since
       mpi.h is a library file);
    3) Replaced "\t" in printf with "---" because OpenMPI considers "\n" or "\t" an
       end of line, and the output will be garbled.

--- tutorials/introduction-to-groups-and-communicators/code/groups.c
+++ tutorials/introduction-to-groups-and-communicators/code/groups.c
@@ -6,51 +6,50 @@
 //
 // Example using MPI_Comm_split to divide a communicator into subcommunicators
 //
-
-#include "mpi.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <mpi.h>
 
 int main(int argc, char **argv) {
-	MPI_Init(NULL, NULL);
+  MPI_Init(NULL, NULL);
 
-	// Get the rank and size in the original communicator
-	int world_rank, world_size;
-	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+  // Get the rank and size in the original communicator
+  int world_rank, world_size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-	// Get the group of processes in MPI_COMM_WORLD
-	MPI_Group world_group;
-	MPI_Comm_group(MPI_COMM_WORLD, &world_group);
+  // Get the group of processes in MPI_COMM_WORLD
+  MPI_Group world_group;
+  MPI_Comm_group(MPI_COMM_WORLD, &world_group);
 
-	int n = 7;
-	const int ranks[7] = {1, 2, 3, 5, 7, 11, 13};
+  int n = 7;
+  const int ranks[7] = {1, 2, 3, 5, 7, 11, 13};
 
-	// Construct a group containing all of the prime ranks in world_group
-	MPI_Group prime_group;
-	MPI_Group_incl(world_group, 7, ranks, &prime_group);
+  // Construct a group containing all of the prime ranks in world_group
+  MPI_Group prime_group;
+  MPI_Group_incl(world_group, 7, ranks, &prime_group);
 
-	// Create a new communicator based on the group
-	MPI_Comm prime_comm;
-	MPI_Comm_create_group(MPI_COMM_WORLD, prime_group, 0, &prime_comm);
+  // Create a new communicator based on the group
+  MPI_Comm prime_comm;
+  MPI_Comm_create_group(MPI_COMM_WORLD, prime_group, 0, &prime_comm);
 
-	int prime_rank = -1, prime_size = -1;
-	// If this rank isn't in the new communicator, it will be MPI_COMM_NULL
-	// Using MPI_COMM_NULL for MPI_Comm_rank or MPI_Comm_size is erroneous
-	if (MPI_COMM_NULL != prime_comm) {
-		MPI_Comm_rank(prime_comm, &prime_rank);
-		MPI_Comm_size(prime_comm, &prime_size);
-	}
+  int prime_rank = -1, prime_size = -1;
+  // If this rank isn't in the new communicator, it will be MPI_COMM_NULL
+  // Using MPI_COMM_NULL for MPI_Comm_rank or MPI_Comm_size is erroneous
+  if (MPI_COMM_NULL != prime_comm) {
+    MPI_Comm_rank(prime_comm, &prime_rank);
+    MPI_Comm_size(prime_comm, &prime_size);
+  }
 
-	printf("WORLD RANK/SIZE: %d/%d \t PRIME RANK/SIZE: %d/%d\n",
-		world_rank, world_size, prime_rank, prime_size);
+  printf("WORLD RANK/SIZE: %d/%d --- PRIME RANK/SIZE: %d/%d\n",
+    world_rank, world_size, prime_rank, prime_size);
 
-	MPI_Group_free(&world_group);
-	MPI_Group_free(&prime_group);
+  MPI_Group_free(&world_group);
+  MPI_Group_free(&prime_group);
 
-	if (MPI_COMM_NULL != prime_comm) {
-		MPI_Comm_free(&prime_comm);
-	}
+  if (MPI_COMM_NULL != prime_comm) {
+    MPI_Comm_free(&prime_comm);
+  }
 
-	MPI_Finalize();
+  MPI_Finalize();
 }
--- tutorials/introduction-to-groups-and-communicators/code/split.c
+++ tutorials/introduction-to-groups-and-communicators/code/split.c
@@ -7,32 +7,32 @@
 // Example using MPI_Comm_split to divide a communicator into subcommunicators
 //
 
-#include "mpi.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <mpi.h>
 
 int main(int argc, char **argv) {
-	MPI_Init(NULL, NULL);
+  MPI_Init(NULL, NULL);
 
-	// Get the rank and size in the original communicator
-	int world_rank, world_size;
-	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+  // Get the rank and size in the original communicator
+  int world_rank, world_size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-	int color = world_rank / 4; // Determine color based on row
+  int color = world_rank / 4; // Determine color based on row
 
-	// Split the communicator based on the color and use the original rank for ordering
-	MPI_Comm row_comm;
-	MPI_Comm_split(MPI_COMM_WORLD, color, world_rank, &row_comm);
+  // Split the communicator based on the color and use the original rank for ordering
+  MPI_Comm row_comm;
+  MPI_Comm_split(MPI_COMM_WORLD, color, world_rank, &row_comm);
 
-	int row_rank, row_size;
-	MPI_Comm_rank(row_comm, &row_rank);
-	MPI_Comm_size(row_comm, &row_size);
+  int row_rank, row_size;
+  MPI_Comm_rank(row_comm, &row_rank);
+  MPI_Comm_size(row_comm, &row_size);
 
-	printf("WORLD RANK/SIZE: %d/%d \t ROW RANK/SIZE: %d/%d\n",
-		world_rank, world_size, row_rank, row_size);
+  printf("WORLD RANK/SIZE: %d/%d --- ROW RANK/SIZE: %d/%d\n",
+    world_rank, world_size, row_rank, row_size);
 
-	MPI_Comm_free(&row_comm);
+  MPI_Comm_free(&row_comm);
 
-	MPI_Finalize();
+  MPI_Finalize();
 }
