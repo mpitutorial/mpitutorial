f23539c77045cb4eac1bfcb57d6bbfdacae2193d
Stephen P. Cook <s.cook4@exeter.ac.uk>
2023-11-30 11:01:02 +0000

    Update bcast examples with subroutines/interfaces

--- tutorials/mpi-fortran/code/compare_bcast.f90
+++ tutorials/mpi-fortran/code/compare_bcast.f90
@@ -1,39 +1,19 @@
-module subs
-  use mpi_f08
-contains
-subroutine my_bcast(data, count, datatype, root, communicator, ierror)
-  integer, intent (in)    :: count, root
-  type(MPI_Comm), intent (in) :: communicator
-  type(MPI_Datatype), intent (in) :: datatype
-  integer, intent (inout) :: data(count)
-  integer, intent (out)   :: ierror
-
-  integer :: world_rank, world_size
-  integer :: i
-
-  call MPI_COMM_SIZE(communicator, world_size, ierror)
-  call MPI_COMM_RANK(communicator, world_rank, ierror)
-
-  if (world_rank .eq. root) then
-    ! If we are the root process, send our data to everyone
-    do i = 0, world_size - 1
-      if (i .ne. world_rank) then
-        call MPI_SEND(data, count, datatype, i, 0, communicator, ierror)
-      end if
-    end do
-  else
-    ! If we are a receiver process, receive the data from the root
-    call MPI_RECV(data, count, datatype, root, 0, communicator, MPI_STATUS_IGNORE, ierror)
-  end if
-end subroutine my_bcast
-end module subs
-
 program main
   use mpi_f08
   use iso_fortran_env, only: error_unit
-  use subs, only: my_bcast
 
   implicit none
+  interface
+    subroutine my_bcast(data, count, datatype, root, communicator, ierror)
+      import MPI_Comm, MPI_Datatype
+      implicit none
+      integer, intent (in)    :: count, root
+      type(MPI_Comm), intent (in) :: communicator
+      type(MPI_Datatype), intent (in) :: datatype
+      integer, intent (inout) :: data(count)
+      integer, intent (out)   :: ierror
+    end subroutine my_bcast
+  end interface
 
   integer              :: num_args, num_elements, num_trials
   character(12)        :: args(2)
@@ -94,3 +74,31 @@ program main
   call MPI_FINALIZE()
 
 end program
+
+subroutine my_bcast(data, count, datatype, root, communicator, ierror)
+  use mpi_f08
+  implicit none
+  integer, intent (in)    :: count, root
+  type(MPI_Comm), intent (in) :: communicator
+  type(MPI_Datatype), intent (in) :: datatype
+  integer, intent (inout) :: data(count)
+  integer, intent (out)   :: ierror
+
+  integer :: world_rank, world_size
+  integer :: i
+
+  call MPI_COMM_SIZE(communicator, world_size, ierror)
+  call MPI_COMM_RANK(communicator, world_rank, ierror)
+
+  if (world_rank .eq. root) then
+    ! If we are the root process, send our data to everyone
+    do i = 0, world_size - 1
+      if (i .ne. world_rank) then
+        call MPI_SEND(data, count, datatype, i, 0, communicator, ierror)
+      end if
+    end do
+  else
+    ! If we are a receiver process, receive the data from the root
+    call MPI_RECV(data, count, datatype, root, 0, communicator, MPI_STATUS_IGNORE, ierror)
+  end if
+end subroutine my_bcast
--- tutorials/mpi-fortran/code/my_bcast.f90
+++ tutorials/mpi-fortran/code/my_bcast.f90
@@ -1,7 +1,45 @@
-module subs
+program main
   use mpi_f08
-contains
+
+  implicit none
+
+  interface
+    subroutine my_bcast(data, count, datatype, root, communicator, ierror)
+      import MPI_Comm, MPI_Datatype
+      implicit none
+      integer, intent (in)    :: count, root
+      type(MPI_Comm), intent (in) :: communicator
+      type(MPI_Datatype), intent (in) :: datatype
+      integer, intent (inout) :: data(count)
+      integer, intent (out)   :: ierror
+    end subroutine my_bcast
+  end interface
+
+  integer :: world_rank, ierror
+  integer :: data(1)
+
+  call MPI_INIT()
+  call MPI_COMM_RANK(MPI_COMM_WORLD, world_rank)
+
+  if (world_rank .eq. 0) then
+    data = 100
+    print '("Process 0 broadcasting data ", I0)', data
+    call my_bcast(data, 1, MPI_INT, 0, MPI_COMM_WORLD, ierror)
+  else
+    call my_bcast(data, 1, MPI_INT, 0, MPI_COMM_WORLD, ierror)
+    print '("Process ", I0, " received data ", I0, " from root process")', &
+      world_rank, data
+  end if
+
+  ! Finalize the MPI environment
+  call MPI_FINALIZE()
+
+end program
+
+
 subroutine my_bcast(data, count, datatype, root, communicator, ierror)
+  use mpi_f08
+  implicit none
   integer, intent (in)    :: count, root
   type(MPI_Comm), intent (in) :: communicator
   type(MPI_Datatype), intent (in) :: datatype
@@ -26,31 +64,3 @@ subroutine my_bcast(data, count, datatype, root, communicator, ierror)
     call MPI_RECV(data, count, datatype, root, 0, communicator, MPI_STATUS_IGNORE, ierror)
   end if
 end subroutine my_bcast
-end module subs
-
-program main
-  use mpi_f08
-  use subs, only: my_bcast
-
-  implicit none
-
-  integer :: world_rank, ierror
-  integer :: data(1)
-
-  call MPI_INIT()
-  call MPI_COMM_RANK(MPI_COMM_WORLD, world_rank)
-
-  if (world_rank .eq. 0) then
-    data = 100
-    print '("Process 0 broadcasting data ", I0)', data
-    call my_bcast(data, 1, MPI_INT, 0, MPI_COMM_WORLD, ierror)
-  else
-    call my_bcast(data, 1, MPI_INT, 0, MPI_COMM_WORLD, ierror)
-    print '("Process ", I0, " received data ", I0, " from root process")', &
-      world_rank, data
-  end if
-
-  ! Finalize the MPI environment
-  call MPI_FINALIZE()
-
-end program
